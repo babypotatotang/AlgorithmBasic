4102bc997c601baa3a171d235e8c1d55dfba0a62
kthink03 <gwonso4502@gmail.com>
2022-04-05 21:28:10 +0900

    0405_Medians modify

--- Chapter 2. Divide and conquer/2.4 Medians.py
+++ Chapter 2. Divide and conquer/2.4 Medians.py
@@ -1,14 +1,31 @@
+
+# k=k-1 #list가 0부터 시작하는 것을 감안 
+# Sl=[]; Sr=[]; Sv=[]
+
+# for i in S:
+#     if i<v: Sl.append(i)
+#     elif i==v: Sv.append(i)
+#     else: Sr.append(i)
+
+# Sl=sorted(Sl); Sr=sorted(Sr); Sv=sorted(Sv)
+
+# if k<=len(Sl): print(Sl[k])
+# elif k>len(Sl) and k<=(len(Sl)+len(Sv)): print(v)
+# elif k>len(Sl)+len(Sv): print(Sr[k-len(Sl)-len(Sv)])
+
 S=[2,36,5,21,8,11,20,5,4,1]; v=5; k=8
-k=k-1 #list가 0부터 시작하는 것을 감안 
-Sl=[]; Sr=[]; Sv=[]
 
-for i in S:
-    if i<v: Sl.append(i)
-    elif i==v: Sv.append(i)
-    else: Sr.append(i)
+def selection(S,k):
+    k=k-1 #list가 0부터 시작하는 것을 감안 
+    Sl=[]; Sr=[]; Sv=[]
 
-Sl=sorted(Sl); Sr=sorted(Sr); Sv=sorted(Sv)
+    for i in S:
+        if i<v: Sl.append(i)
+        elif i==v: Sv.append(i)
+        else: Sr.append(i)
 
-if k<=len(Sl): print(Sl[k])
-elif k>len(Sl) and k<=(len(Sl)+len(Sv)): print(v)
-elif k>len(Sl)+len(Sv): print(Sr[k-len(Sl)-len(Sv)])
+    if k<=len(Sl): selection(Sl,k)
+    elif k>len(Sl) and k<=(len(Sl)+len(Sv)): return v
+    elif k>len(Sl)+len(Sv): selection(Sr,k-len(Sl)-len(Sv))
+
+print(selection(S,k))
